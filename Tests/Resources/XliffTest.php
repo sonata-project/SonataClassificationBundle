5e3de071bd577a5410730ddae7b50440d16d9db0
Thomas Rabaix <thomas.rabaix@gmail.com>
2014-07-31 10:58:34 +0200

    fix namespace

--- Tests/Resources/XliffTest.php
+++ Tests/Resources/XliffTest.php
@@ -8,7 +8,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Sonata\AdminBundle\Tests\Resources;
+namespace Sonata\ClassificationBundle\Tests\Resources;
 
 
 use Sonata\CoreBundle\Test\XliffValidatorTestCase;

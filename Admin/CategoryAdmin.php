6efc40e75540594d88d3c609cb0cc5cbdeef1d2b
Daniel González <dgzaballos@undanet.com>
2015-09-25 12:44:16 +0200

    fixed GET parameters overriding

--- Admin/CategoryAdmin.php
+++ Admin/CategoryAdmin.php
@@ -164,6 +164,8 @@ class CategoryAdmin extends Admin
             'hide_context' => $this->hasRequest() ? (int) $this->getRequest()->get('hide_context', 0) : 0,
         );
 
+        $parameters = array_merge(parent::getPersistentParameters(), $parameters);
+
         if ($this->getSubject()) {
             $parameters['context'] = $this->getSubject()->getContext() ? $this->getSubject()->getContext()->getId() : '';
 

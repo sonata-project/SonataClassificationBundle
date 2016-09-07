944f94333ba31511ffb47bc201c5d88bb9ca4422
Peter Pierre <binh@sunrise.vn>
2016-09-08 05:17:52 +0700

    Fix old Admin Class Usage for Context Admin and provide Tests for this case

--- Tests/Admin/AdminTest.php
+++ Tests/Admin/AdminTest.php
@@ -11,6 +11,7 @@
 
 namespace Sonata\ClassificationBundle\Tests\Admin;
 
+use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\ClassificationBundle\Model\ContextManagerInterface;
 
 class AdminTest extends \PHPUnit_Framework_TestCase
@@ -23,6 +24,18 @@ class AdminTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->contextManager = $this->getMock('Sonata\ClassificationBundle\Model\ContextManagerInterface');
+    }
+
+    public function testAbstractAdminChildren()
+    {
+        $contextAwareAdmin = $this->getMockBuilder('Sonata\ClassificationBundle\Admin\ContextAwareAdmin')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->assertInstanceOf(AbstractAdmin::class, $contextAwareAdmin);
+        $contextAdmin = $this->getMockBuilder('Sonata\ClassificationBundle\Admin\ContextAdmin')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->assertInstanceOf(AbstractAdmin::class, $contextAdmin);
     }
 
     public function testGetPersistentParametersWithNoExtension()

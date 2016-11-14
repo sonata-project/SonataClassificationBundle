911b40838fff4ceef894edfa324f268c8866f48d
lemorragia <simone@aperturelabs.it>
2016-11-14 11:36:52 +0100

    added support for FosRestBundle 2.0 (#245)
    * added support for FosRest 2.0
    * fixed composer.json error on restbundle minimum version

--- Controller/Api/ContextController.php
+++ Controller/Api/ContextController.php
@@ -11,9 +11,11 @@
 
 namespace Sonata\ClassificationBundle\Controller\Api;
 
+use FOS\RestBundle\Context\Context;
 use FOS\RestBundle\Controller\Annotations\QueryParam;
 use FOS\RestBundle\Controller\Annotations\View;
 use FOS\RestBundle\Request\ParamFetcherInterface;
+use FOS\RestBundle\View\View as FOSRestView;
 use JMS\Serializer\SerializationContext;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Sonata\ClassificationBundle\Model\ContextInterface;
@@ -255,11 +257,18 @@ class ContextController
             $context = $form->getData();
             $this->contextManager->save($context);
 
-            $view = \FOS\RestBundle\View\View::create($context);
-            $serializationContext = SerializationContext::create();
-            $serializationContext->setGroups(array('sonata_api_read'));
-            $serializationContext->enableMaxDepthChecks();
-            $view->setSerializationContext($serializationContext);
+            $view = FOSRestView::create($context);
+
+            if (class_exists('FOS\RestBundle\Context\Context')) {
+                $context = new Context();
+                $context->setGroups(array('sonata_api_read'));
+                $view->setContext($context);
+            } else {
+                $serializationContext = SerializationContext::create();
+                $serializationContext->setGroups(array('sonata_api_read'));
+                $serializationContext->enableMaxDepthChecks();
+                $view->setSerializationContext($serializationContext);
+            }
 
             return $view;
         }

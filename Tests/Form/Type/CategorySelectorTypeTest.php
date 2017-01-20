a9155013a4a0f5c19385fbc575f6f38d617c873b
mkindratko <mkindratko@grossum.com>
2017-01-20 15:49:47 +0200

    fixed code style

--- Tests/Form/Type/CategorySelectorTypeTest.php
+++ Tests/Form/Type/CategorySelectorTypeTest.php
@@ -5,6 +5,14 @@ namespace Sonata\ClassificationBundle\Tests\Form\Type;
 use Sonata\ClassificationBundle\Form\Type\CategorySelectorType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+/**
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 class CategorySelectorTypeTest extends \PHPUnit_Framework_TestCase
 {
     public function testConfigureOptions()

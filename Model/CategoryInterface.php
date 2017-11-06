30e40be6b59d22913a345416b6614161aaac2dbb
Christian Gripp <mail@core23.de>
2017-11-06 19:03:27 +0000

    Apply fixes from StyleCI

--- Model/CategoryInterface.php
+++ Model/CategoryInterface.php
@@ -87,7 +87,7 @@ interface CategoryInterface
      * @param CategoryInterface $children
      * @param bool              $nested
      */
-    public function addChild(CategoryInterface $children, $nested = false);
+    public function addChild(self $children, $nested = false);
 
     /**
      * Get Children.
@@ -116,7 +116,7 @@ interface CategoryInterface
      * @param CategoryInterface $parent
      * @param bool              $nested
      */
-    public function setParent(CategoryInterface $parent = null, $nested = false);
+    public function setParent(self $parent = null, $nested = false);
 
     /**
      * Get Parent.

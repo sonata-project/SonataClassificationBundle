f268eb53799d5362a7de6cd4c5b385b6c97ccf58
Sullivan SENECHAL <soullivaneuh@gmail.com>
2018-09-06 02:06:08 +0200

    Applied fixes from FlintCI

--- src/Block/Service/AbstractCategoriesBlockService.php
+++ src/Block/Service/AbstractCategoriesBlockService.php
@@ -206,7 +206,7 @@ abstract class AbstractCategoriesBlockService extends AbstractClassificationBloc
     {
         $block->setSetting(
             'categoryId',
-            is_object($block->getSetting('categoryId')) ? $block->getSetting('categoryId')->getId() : null
+            \is_object($block->getSetting('categoryId')) ? $block->getSetting('categoryId')->getId() : null
         );
     }
 }

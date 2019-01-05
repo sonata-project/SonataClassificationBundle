3fe21f2e51bfa48786f7b42a1ed713cdf75a14cb
Grégoire Paris <postmaster@greg0ire.fr>
2019-01-05 14:14:44 +0100

    Move classes to their own files

---
+++ tests/Entity/CategoryTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\ClassificationBundle\Tests\Entity;
+
+use Sonata\ClassificationBundle\Entity\BaseCategory;
+
+abstract class CategoryTest extends BaseCategory
+{
+    private $id;
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+}

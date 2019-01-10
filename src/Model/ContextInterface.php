9c10a3572605d0ef7f9afd429e41715a6f6c8ff7
Sullivan SENECHAL <soullivaneuh@gmail.com>
2019-01-10 02:06:07 +0100

    Applied fixes from FlintCI

--- src/Model/ContextInterface.php
+++ src/Model/ContextInterface.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of the Sonata Project package.
  *
@@ -13,7 +15,7 @@ namespace Sonata\ClassificationBundle\Model;
 
 interface ContextInterface
 {
-    const DEFAULT_CONTEXT = 'default';
+    public const DEFAULT_CONTEXT = 'default';
 
     /**
      * Set name.

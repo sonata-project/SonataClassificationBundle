2705072f1152a544267a29b2f5a193c69ccb6af3
Vincent Langlet <vincentlanglet@hotmail.fr>
2021-06-15 12:34:42 +0200

    Add compatibility for command and controller

--- src/Command/FixContextCommand.php
+++ src/Command/FixContextCommand.php
@@ -13,47 +13,83 @@ declare(strict_types=1);
 
 namespace Sonata\ClassificationBundle\Command;
 
+use Sonata\ClassificationBundle\Model\CategoryManagerInterface;
+use Sonata\ClassificationBundle\Model\CollectionManagerInterface;
 use Sonata\ClassificationBundle\Model\ContextInterface;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Sonata\ClassificationBundle\Model\ContextManagerInterface;
+use Sonata\ClassificationBundle\Model\TagManagerInterface;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class FixContextCommand extends ContainerAwareCommand
+class FixContextCommand extends Command
 {
+    protected static $defaultName = 'sonata:classification:fix-context';
+    protected static $defaultDescription = 'Generate the default context if none defined and attach the context to all elements';
+
+    /**
+     * @var ContextManagerInterface
+     */
+    private $contextManager;
+
+    /**
+     * @var TagManagerInterface
+     */
+    private $tagManager;
+
+    /**
+     * @var CollectionManagerInterface
+     */
+    private $collectionManager;
+
+    /**
+     * @var CategoryManagerInterface
+     */
+    private $categoryManager;
+
+    public function __construct(
+        ContextManagerInterface $contextManager,
+        TagManagerInterface $tagManager,
+        CollectionManagerInterface $collectionManager,
+        CategoryManagerInterface $categoryManager
+    )
+    {
+        parent::__construct();
+
+        $this->contextManager = $contextManager;
+        $this->tagManager = $tagManager;
+        $this->collectionManager = $collectionManager;
+        $this->categoryManager = $categoryManager;
+    }
+
     public function configure(): void
     {
-        $this->setName('sonata:classification:fix-context');
-        $this->setDescription('Generate the default context if none defined and attach the context to all elements');
+        $this->setDescription(static::$defaultDescription);
     }
 
     public function execute(InputInterface $input, OutputInterface $output): void
     {
-        $contextManager = $this->getContainer()->get('sonata.classification.manager.context');
-        $tagManager = $this->getContainer()->get('sonata.classification.manager.tag');
-        $collectionManager = $this->getContainer()->get('sonata.classification.manager.collection');
-        $categoryManager = $this->getContainer()->get('sonata.classification.manager.category');
-
         $output->writeln('1. Checking default context');
 
-        $defaultContext = $contextManager->findOneBy([
+        $defaultContext = $this->contextManager->findOneBy([
             'id' => ContextInterface::DEFAULT_CONTEXT,
         ]);
 
         if (!$defaultContext) {
             $output->writeln(' > default context is missing, creating one');
-            $defaultContext = $contextManager->create();
+            $defaultContext = $this->contextManager->create();
             $defaultContext->setId(ContextInterface::DEFAULT_CONTEXT);
             $defaultContext->setName('Default');
             $defaultContext->setEnabled(true);
 
-            $contextManager->save($defaultContext);
+            $this->contextManager->save($defaultContext);
         } else {
             $output->writeln(' > default context exists');
         }
 
         $output->writeln('2. Find tag without default context');
 
-        foreach ($tagManager->findBy([]) as $tag) {
+        foreach ($this->tagManager->findBy([]) as $tag) {
             if ($tag->getContext()) {
                 continue;
             }
@@ -61,12 +97,12 @@ class FixContextCommand extends ContainerAwareCommand
             $output->writeln(sprintf(' > attach default context to tag: %s (%s)', $tag->getSlug(), $tag->getId()));
             $tag->setContext($defaultContext);
 
-            $tagManager->save($tag);
+            $this->tagManager->save($tag);
         }
 
         $output->writeln('3. Find collection without default context');
 
-        foreach ($collectionManager->findBy([]) as $collection) {
+        foreach ($this->collectionManager->findBy([]) as $collection) {
             if ($collection->getContext()) {
                 continue;
             }
@@ -74,12 +110,12 @@ class FixContextCommand extends ContainerAwareCommand
             $output->writeln(sprintf(' > attach default context to collection: %s (%s)', $collection->getSlug(), $collection->getId()));
             $collection->setContext($defaultContext);
 
-            $collectionManager->save($collection);
+            $this->collectionManager->save($collection);
         }
 
         $output->writeln('3. Find category without default context');
 
-        foreach ($categoryManager->findBy([]) as $category) {
+        foreach ($this->categoryManager->findBy([]) as $category) {
             if ($category->getContext()) {
                 continue;
             }
@@ -87,7 +123,7 @@ class FixContextCommand extends ContainerAwareCommand
             $output->writeln(sprintf(' > attach default context to collection: %s (%s)', $category->getSlug(), $category->getId()));
             $category->setContext($defaultContext);
 
-            $categoryManager->save($category);
+            $this->categoryManager->save($category);
         }
 
         $output->writeln('Done!');

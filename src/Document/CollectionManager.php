c7a1ce35923504ff2cccc2dc39a5258cf38924d5
Javier Spagnoletti <phansys@gmail.com>
2021-09-08 06:30:50 -0300

    Fix ODM queries

--- src/Document/CollectionManager.php
+++ src/Document/CollectionManager.php
@@ -22,17 +22,10 @@ class CollectionManager extends BaseDocumentManager implements CollectionManager
 {
     public function getPager(array $criteria, $page, $limit = 10, array $sort = [])
     {
-        $parameters = [];
-
-        $query = $this->getRepository()
-            ->createQueryBuilder('t')
-            ->select('t');
-
-        $criteria['enabled'] = $criteria['enabled'] ?? true;
-        $query->andWhere('t.enabled = :enabled');
-        $parameters['enabled'] = $criteria['enabled'];
-
-        $query->setParameters($parameters);
+        $query = $this->getDocumentManager()
+            ->createQueryBuilder($this->getClass())
+            ->field('enabled')
+            ->equals($criteria['enabled'] ?? true);
 
         $pager = new Pager();
         $pager->setMaxPerPage($limit);

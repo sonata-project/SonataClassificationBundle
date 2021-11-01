dea4741949694dc80c0c0deeca7455e87601396e
Vincent Langlet <vincentlanglet@hotmail.fr>
2021-11-01 21:02:51 +0100

    Deprecate disableChildrenLazyLoading

--- src/Entity/BaseCategory.php
+++ src/Entity/BaseCategory.php
@@ -18,8 +18,19 @@ use Sonata\ClassificationBundle\Model\Category as ModelCategory;
 
 abstract class BaseCategory extends ModelCategory
 {
+    /**
+     * @deprecated since sonata-project/classification-bundle 3.x, to be removed in 4.0.
+     */
     public function disableChildrenLazyLoading()
     {
+        @trigger_error(
+            sprintf(
+                'The method %s is deprecated since sonata-project/classification-bundle 3.x and will be removed in 4.0.',
+                __METHOD__
+            ),
+            \E_USER_DEPRECATED
+        );
+
         if ($this->children instanceof PersistentCollection) {
             $this->children->setInitialized(true);
         }

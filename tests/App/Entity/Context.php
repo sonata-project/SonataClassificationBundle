03730aea59b21fe3d25ad2296d5a62ecc3855684
Vincent Langlet <vincentlanglet@hotmail.fr>
2023-03-08 23:11:13 +0100

    Remove annotations

--- tests/App/Entity/Context.php
+++ tests/App/Entity/Context.php
@@ -17,18 +17,10 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Sonata\ClassificationBundle\Entity\BaseContext;
 
-/**
- * @ORM\Entity
- * @ORM\Table(name="classification__context")
- */
 #[ORM\Entity]
 #[ORM\Table(name: 'classification__context')]
 class Context extends BaseContext
 {
-    /**
-     * @ORM\Id
-     * @ORM\Column(type="string")
-     */
     #[ORM\Id]
     #[ORM\Column(type: Types::STRING)]
     protected ?string $id = null;
